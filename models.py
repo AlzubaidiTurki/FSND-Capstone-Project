517cc4b39f2c0c32c1a444e341cbc24e2397c3ed
تُركي الزُّبيدي <35930561+AlzubaidiTurki@users.noreply.github.com>
2021-09-06 19:21:19 +0300

    update DB_PATH

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', 'postgres')  
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
 DB_NAME = os.getenv('DB_NAME', 'fate')  
-DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_PATH = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 db = SQLAlchemy()
 
 def setup_db(app, database_path=DB_PATH):
