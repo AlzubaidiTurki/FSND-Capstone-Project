7b679c8a6ce388c50ae9d1d3ae45d6b3ad7c8c7d
تُركي الزُّبيدي <35930561+AlzubaidiTurki@users.noreply.github.com>
2021-09-10 14:39:38 +0300

    format doc

--- models.py
+++ models.py
@@ -2,13 +2,15 @@ from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_migrate import Migrate
-import os 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
-DB_USER = os.getenv('DB_USER', 'postgres')  
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
-DB_NAME = os.getenv('DB_NAME', 'fate')  
-DB_PATH = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+import os
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'fate')
+DB_PATH = 'postgresql://{}:{}@{}/{}'.format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 db = SQLAlchemy()
+
 
 def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -22,11 +24,13 @@ class Servant(db.Model):
     __tablename__ = 'servant'
     id = Column(Integer(), primary_key=True)
     name = Column(String(), nullable=False)
-    type = Column(String()) # Archer, Saber, etc..
-    source = Column(String()) # Where has the servant taken from (real life figure? a book? etc)
-    image = Column(String()) # insert an image of the character.
+    type = Column(String())  # Archer, Saber, etc..
+    # Where has the servant taken from (real life figure? a book? etc)
+    source = Column(String())
+    image = Column(String())  # insert an image of the character.
     master_id = Column(Integer, ForeignKey('master.id'))
     master = relationship('Master', back_populates='servant')
+
     def insert(self):
         db.session.add(self)
         db.session.commit()
@@ -37,7 +41,7 @@ class Servant(db.Model):
 
     def update(self):
         db.session.commit()
-    
+
     def format(self):
         return {
             'name': self.name,
@@ -45,36 +49,43 @@ class Servant(db.Model):
             'source': self.source,
             'master': self.master_id
         }
-    
+
     def __repr__(self):
         return f'Name: {self.name}, Type: {self.type}, Source: {self.source}, image: {self.image}'
+
 
 class Master(db.Model):
     __tablename__ = 'master'
     id = Column(Integer(), primary_key=True)
     name = Column(String(), nullable=False)
-    image = Column(String()) # insert an image of the character.
-    servant = relationship("Servant", back_populates="master", cascade="all,delete", uselist=False) # One Master can have only one servant.
+    image = Column(String())  # insert an image of the character.
+    # One Master can have only one servant.
+    servant = relationship(
+        "Servant",
+        back_populates="master",
+        cascade="all,delete",
+        uselist=False)
 
     def insert(self):
         db.session.add(self)
         db.session.commit()
-        
+
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
     def update(self):
         db.session.commit()
-    
+
     def format(self):
         return {
             'name': self.name,
             'image': self.image
         }
-    
+
     def __repr__(self):
         return f'Name: {self.name}, Image: {self.image}'
+
 
 if __name__ == '__main__':
     print("fine")
--- seeder.py
+++ seeder.py
@@ -4,26 +4,55 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 db = SQLAlchemy()
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
-DB_USER = os.getenv('DB_USER', 'postgres')  
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
-DB_NAME = os.getenv('DB_NAME', 'fate')  
-DB_PATH = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'fate')
+DB_PATH = 'postgresql://{}:{}@{}/{}'.format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_PATH
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 with app.app_context():
     db.create_all()
-    emiya = Master(id=1,name='Emiya Kiritsugu', image='https://www.gwigwi.com/wp-content/uploads/2016/06/kiritsugu-emiya-e1466929284783.jpg')
-    tohsaka = Master(id=2, name= 'Tohsaka Rin', image='https://2img.net/h/pre15.deviantart.net/75f5/th/pre/i/2015/079/1/0/rin_tohsaka_by_deikawn-d8fer1i.png')
-    kirei = Master(id=3,name= 'Kotomine Kirei', image='https://static.myfigurecollection.net/pics/encyclopedia/4380.jpg?rev=1297513293')
+    emiya = Master(
+        id=1,
+        name='Emiya Kiritsugu',
+        image='https://www.gwigwi.com/wp-content/uploads/2016/06/kiritsugu-emiya-e1466929284783.jpg')
+    tohsaka = Master(
+        id=2,
+        name='Tohsaka Rin',
+        image='https://2img.net/h/pre15.deviantart.net/75f5/th/pre/i/2015/079/1/0/rin_tohsaka_by_deikawn-d8fer1i.png')
+    kirei = Master(
+        id=3,
+        name='Kotomine Kirei',
+        image='https://static.myfigurecollection.net/pics/encyclopedia/4380.jpg?rev=1297513293')
     db.session.add(emiya)
     db.session.add(tohsaka)
     db.session.add(kirei)
     db.session.commit()
-    archer = Servant(id=1, name='Gilgamesh', type = 'Archer', source='Epic of Gilgamesh', image='', master_id = Master.query.filter_by(name='Tohsaka Rin').first().id)
-    saber = Servant(id=2, name='Artoria Pendragon', type = 'Saber',  source='Arthurian Legends', image='', master_id = Master.query.filter_by(name='Emiya Kiritsugu').first().id)
-    caster = Servant(id=3, name="Gilles de Rais", type = "Caster",  source='Historical Fact', image='https://static.myfigurecollection.net/pics/encyclopedia/32393.jpg?rev=1494897149')
+    archer = Servant(
+        id=1,
+        name='Gilgamesh',
+        type='Archer',
+        source='Epic of Gilgamesh',
+        image='',
+        master_id=Master.query.filter_by(
+            name='Tohsaka Rin').first().id)
+    saber = Servant(
+        id=2,
+        name='Artoria Pendragon',
+        type='Saber',
+        source='Arthurian Legends',
+        image='',
+        master_id=Master.query.filter_by(
+            name='Emiya Kiritsugu').first().id)
+    caster = Servant(
+        id=3,
+        name="Gilles de Rais",
+        type="Caster",
+        source='Historical Fact',
+        image='https://static.myfigurecollection.net/pics/encyclopedia/32393.jpg?rev=1494897149')
     db.session.add(archer)
     db.session.add(saber)
     db.session.add(caster)
